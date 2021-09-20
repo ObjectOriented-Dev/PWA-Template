935fa74958408f3e32048823762b4e49fa1b37e1
AhotoO <84821980+ObjectOriented-Dev@users.noreply.github.com>
2021-09-20 13:59:12 -0600

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,9 +1,18 @@
+const cacheName = 'pwa_template-v1';
+const cacheContent = [
+    '/PWA-Template/index.html',
+    '/PWA-Template/icon.png',
+    '/PWA-Template/public/app.js',
+    '/PWA-Template/manifest.json',
+    '/PWA-Template/README.md'
+];
+
 self.addEventListener("install", e => {
-    e.waitUntil(
-        caches.open("static").then(cache =>{
-            return cache.addAll(["./public", "./icon.png"]);
-        })
-    );
+    console.log('[Service Worker] Install');
+    e.waitUntil(async() => {
+        const cache = await caches.open(cacheName);
+        await cache.addAll(cacheContent);
+    });
 });
 self.addEventListener("fetch", e =>{
     console.log(`intercep fetch request for: ${e.request.url}`);
