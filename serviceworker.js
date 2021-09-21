a165dcbddbe3bd2050c13f9225a6eee59da6e391
AhotoO <84821980+ObjectOriented-Dev@users.noreply.github.com>
2021-09-21 09:27:42 -0600

    updated service worker so it actually caches the site date

--- serviceworker.js
+++ serviceworker.js
@@ -1,19 +1,21 @@
 const cacheName = 'pwa_template-v1';
 const cacheContent = [
-    '/index.html',
-    '/icon.png',
-    '/public/app.js',
-    '/manifest.json',
-    '/README.md'
+    './index.html',
+    './icon.png',
+    './public/app.js',
+    './manifest.json',
+    './README.md'
 ];
+
 
 self.addEventListener("install", e => {
     console.log('[Service Worker] Install');
-    e.waitUntil(async() => {
-        caches.open(cacheName).then(function(cache){
-            cache.addAll(cacheContent);
+    const cacheUp = async() => {
+        caches.open(cacheName).then((cache)=>{
+            return cache.addAll(cacheContent);
         }); 
-    });
+    }
+    e.waitUntil(cacheUp());
 });
 self.addEventListener("fetch", e =>{
     console.log(`intercep fetch request for: ${e.request.url}`);
